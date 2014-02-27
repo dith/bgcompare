8b9fe5a8ccb458b286823b959d59fe6c9009669e
dith0930@gmail.com <dev@dev-studio2.(none)>
2014-02-27 11:11:04 +0800

    BgCompare Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BgCompare::Application.config.secret_key_base = 'f61383921c3308ad335e72d93612e90c6af25177310014016d723c351228228ed9a302db8472bba4441a41c942fa8064fd462c1d0ad8fcc5725aef4b935a7998'
