0932a97b2ebf1baf7641c5e3143b53839033efbc
dith0930@gmail.com <dev@dev-studio2.(none)>
2014-03-07 10:22:22 +0800

    added BGG api call feature to do searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
-class SearchController < ApplicationController
+require 'net/http'
+require 'json'
 
+class SearchController < ApplicationController
   def index
+    return if params[:search].class == NilClass || params[:search].length == 0
+
+    url = "http://www.boardgamegeek.com/xmlapi2/search?query=" + params[:search].gsub(/\s+/, '+')
+    response = Net::HTTP.get_response(URI.parse(url))
+    @data = Hash.from_xml(response.body)["items"]["item"]
+
   end
 end
